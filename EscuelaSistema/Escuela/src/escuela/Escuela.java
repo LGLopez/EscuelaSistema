cf7973a5ca80b158b0b49f99726cf952129bd296
LGLopez <DARKdestruktion@gmail.com>
2019-09-16 23:05:37 -0500

    primer commit

---
+++ EscuelaSistema/Escuela/src/escuela/Escuela.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escuela;
+
+/**
+ *
+ * @author destr
+ */
+public class Escuela {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Registrar().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
---
+++ EscuelaSistema/Escuela/src/escuela/Usuario.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escuela;
+
+/**
+ *
+ * @author destr
+ */
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String aPaterno;
+    private String aMaterno;
+    private String nombreUsuario;
+    private String password;
+    private int perfil;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getaPaterno() {
+        return aPaterno;
+    }
+
+    public void setaPaterno(String aPaterno) {
+        this.aPaterno = aPaterno;
+    }
+
+    public String getaMaterno() {
+        return aMaterno;
+    }
+
+    public void setaMaterno(String aMaterno) {
+        this.aMaterno = aMaterno;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(int perfil) {
+        this.perfil = perfil;
+    }
+    
+    
+    
+    
+}
